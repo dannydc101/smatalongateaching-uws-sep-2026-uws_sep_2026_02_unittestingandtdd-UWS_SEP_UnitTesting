df8af1d025509cb9710da79a65569bf95de92107
dannydc101 <dannycox101@hotmail.co.uk>
2026-02-05 12:03:46 +0000

    Add files via upload

--- Excercise1/Calculator.java
+++ Excercise1/Calculator.java
@@ -1,22 +1,25 @@
-package Excercise1;
-
+// Calculator.java
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-    
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return (double) a / b;
-    }
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public double divide(int a, int b) {
+		if (b == 0) {
+			throw new ArithmeticException("Cannot divide by zero");
+		}
+		return (double) a / b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Calculator class loaded");
+	}
 }
